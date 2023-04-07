ffdf9c82dac1860540f3795de6fbf93b46d6195d
dcast3ll <dcastell@gmail.com>
2023-04-07 18:21:44 -0400

    toqueteé hellogit 3

--- hellogit3.py
+++ hellogit3.py
@@ -1,2 +1,2 @@
-print('Hello world 3m con cambios V2, ya este cambio no lo sabe los que están haciendo el login')
+print('tocando hellogit3 a pesar que no es mi trabajo. Hello world 3m con cambios V2, ya este cambio no lo sabe los que están haciendo el login')
 #continuamos con practicas
